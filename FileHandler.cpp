334c89f6f0c55e122701ec5e1135e0999b132d8a
DEVS0158 <dpatel6936@conestogac.on.ca>
2024-02-07 14:12:12 -0500

    added md5 function in our project

---
+++ FileHandler.cpp
@@ -0,0 +1,27 @@
+#include "FileHandler.h"
+#include "openssl/md5.h"
+
+
+FileHandler::FileHandler()
+{
+}
+
+bool FileHandler::GetFileInfo(const std::string& filePath, std::string& fileName, size_t& fileSize)
+{
+	return false;
+}
+
+bool FileHandler::ReadFileContent(const std::string& filePath, std::vector<char>& fileContent)
+{
+	return false;
+}
+
+uint32_t FileHandler::CalculateMD5(const std::vector<char>& data)
+{
+	return 0;
+}
+
+uint32_t FileHandler::CalculateMD5Internal(const char* data, size_t size)
+{
+	return 0;
+}
